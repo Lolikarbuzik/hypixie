979859bc8c8778f40c115876a397f597425e1bda
Lolikarbuzik <73756261+Lolikarbuzik@users.noreply.github.com>
2024-05-21 20:27:42 +0200

    fixed issue with only 1 value returned

--- index.js
+++ index.js
@@ -1,31 +1,32 @@
-"use strict"
+"use strict";
 
-const ky = require("ky-universal")
-const isUUID = require("is-uuid")
+const ky = require("ky-universal");
+const isUUID = require("is-uuid");
 
 class HypixelError extends Error {
 	constructor(message = "", ...args) {
-		super(message, ...args)
-		this.message = message
+		super(message, ...args);
+		this.message = message;
 	}
 }
 
 module.exports = async (endpoint, options = {}) => {
 	if (!isUUID.v4(options.key)) {
-		throw new TypeError("`options.key` must be set to an API key!")
+		throw new TypeError("`options.key` must be set to an API key!");
 	}
 
 	const request = await ky(endpoint, {
 		prefixUrl: "https://api.hypixel.net",
-		searchParams: options
-	})
-	const data = await request.json()
+		searchParams: options,
+	});
+	const data = await request.json();
 
 	if (!data.success) {
-		throw new HypixelError(data.cause)
+		throw new HypixelError(data.cause);
 	}
 
-	return Object.values(data)[1]
-}
+	return data;
+	// return Object.values(data)[1]
+};
 
-module.exports.HypixelError = HypixelError
+module.exports.HypixelError = HypixelError;
--- test.js
+++ test.js
@@ -1,14 +1,20 @@
-const test = require("ava")
-const hypixie = require(".")
+const test = require("ava");
+const hypixie = require(".");
 
-test("main", async t => {
+test("main", async (t) => {
 	if (!process.env.HYPIXEL_KEY) {
-		console.warn("Set the HYPIXEL_KEY environment variable in order to test.")
-		return t.pass()
+		console.warn("Set the HYPIXEL_KEY environment variable in order to test.");
+		return t.pass();
 	}
 
-	const key = process.env.HYPIXEL_KEY
+	const key = process.env.HYPIXEL_KEY;
 
-	const { displayname } = await hypixie("player", { uuid: "56da43a4-088d-4a76-82b6-dd431535015e", key })
-	t.is(displayname, "Richienb")
-})
+	const response = await hypixie("player", {
+		uuid: "56da43a4-088d-4a76-82b6-dd431535015e",
+		key,
+	});
+	console.log(response);
+
+	// const { displayname } = await hypixie("player", { uuid: "56da43a4-088d-4a76-82b6-dd431535015e", key })
+	// t.is(displayname, "Richienb")
+});
