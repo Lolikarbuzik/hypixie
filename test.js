bc505693ba507d15f9d73b910e75b6e3e6783091
Lolikarbuzik <73756261+Lolikarbuzik@users.noreply.github.com>
2024-05-21 20:31:43 +0200

    fixed test cases for new response

--- test.js
+++ test.js
@@ -1,20 +1,17 @@
-const test = require("ava");
+const key = process.env.HYPIXEL_KEY;
 const hypixie = require(".");
+const test = require("ava");
 
-test("main", async (t) => {
+test("main", async t => {
 	if (!process.env.HYPIXEL_KEY) {
 		console.warn("Set the HYPIXEL_KEY environment variable in order to test.");
 		return t.pass();
 	}
 
-	const key = process.env.HYPIXEL_KEY;
-
 	const response = await hypixie("player", {
 		uuid: "56da43a4-088d-4a76-82b6-dd431535015e",
-		key,
+		key
 	});
-	console.log(response);
-
-	// const { displayname } = await hypixie("player", { uuid: "56da43a4-088d-4a76-82b6-dd431535015e", key })
-	// t.is(displayname, "Richienb")
+	const { displayname } = response.player;
+	t.is(displayname, "Richienb");
 });
